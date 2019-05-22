d885b54a3c7abfdf69a3805d94c128e759f658be
AnnabellAnastasia <oav2008@gmail.com>
2019-05-22 15:12:32 -0400

    server

--- views/server.js
+++ views/server.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var express = require('body-parser');
 var express = require('method-override');
+var exphbs = require('express-handlebars');
 
 var app = express();
+app.use(express.statistic(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'));
+app.engine('handlebars',exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var port = 3000;
+app.listen(port);
